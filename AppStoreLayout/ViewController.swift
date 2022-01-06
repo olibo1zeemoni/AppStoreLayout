b6a1db4099e440f9ab426ef65ea3b38875a9b634
Zeemoni <gaboahene1@gmail.com>
2022-01-06 04:48:18 +0000

    final commit

---
+++ AppStoreLayout/ViewController.swift
@@ -0,0 +1,191 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+        
+    // MARK: Section Definitions
+    enum Section: Hashable {
+        case promoted
+        case standard(String)
+        case categories
+    }
+    
+    enum SupplementaryViewKind{
+        static let header = "header"
+        static let topLine = "topLine"
+        static let bottomLine = "bottomLine"
+    }
+
+    @IBOutlet var collectionView: UICollectionView!
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    var sections = [Section]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.register(PromotedAppCollectionViewCell.self, forCellWithReuseIdentifier: PromotedAppCollectionViewCell.reuseIdentifier)
+        collectionView.register(StandardAppCollectionViewCell.self, forCellWithReuseIdentifier: StandardAppCollectionViewCell.reuseIdentifier)
+        collectionView.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: CategoryCollectionViewCell.reuseIdentifier)
+        
+        collectionView.register(SectionHeaderView.self, forSupplementaryViewOfKind: SupplementaryViewKind.header, withReuseIdentifier: SectionHeaderView.reuseIdentifier)
+        collectionView.register(LineView.self, forSupplementaryViewOfKind: SupplementaryViewKind.bottomLine, withReuseIdentifier: LineView.reuseIdentifier)
+        collectionView.register(LineView.self, forSupplementaryViewOfKind: SupplementaryViewKind.topLine, withReuseIdentifier: LineView.reuseIdentifier)
+        // MARK: Collection View Setup
+        collectionView.collectionViewLayout = createLayout()
+                
+        configureDataSource()
+    }
+    
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout{ (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let section = self.sections[sectionIndex]
+            
+            let supplementaryLineInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
+            let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.92), heightDimension: .absolute(44))
+            let headerItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: SupplementaryViewKind.header, alignment: .top)
+            headerItem.contentInsets = supplementaryLineInsets
+            
+            let lineHeight = 1 / layoutEnvironment.traitCollection.displayScale
+            let lineItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.92), heightDimension: .absolute(lineHeight))
+            
+            let topLineItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: lineItemSize, elementKind: SupplementaryViewKind.topLine, alignment: .top)
+            topLineItem.contentInsets = supplementaryLineInsets
+            let bottomLineItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: lineItemSize, elementKind: SupplementaryViewKind.bottomLine, alignment: .bottom)
+            bottomLineItem.contentInsets = supplementaryLineInsets
+          
+            
+            
+            
+            switch section {
+            case .promoted:
+                //MARK: Promoted Section Layout
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
+                
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.92), heightDimension: .estimated(300))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPagingCentered
+                section.boundarySupplementaryItems = [topLineItem,bottomLineItem]
+                section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 4, bottom: 20, trailing: 0)
+                
+                return section
+            case .standard:
+                //MARK: Standard Section Layout
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1/3))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
+                
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.92), heightDimension: .estimated(250))
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 3)
+                let section = NSCollectionLayoutSection(group: group)
+                
+                
+                section.orthogonalScrollingBehavior = .groupPagingCentered
+                section.boundarySupplementaryItems = [headerItem, bottomLineItem]
+                section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 4, bottom: 20, trailing: 0)
+                
+                return section
+            case .categories:
+                //MARK: Categories Section Layout
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                let availableWidth = layoutEnvironment.container.effectiveContentSize.width
+                let groupWidth = availableWidth * 0.92
+                let remainingWidth = (availableWidth - groupWidth)/2
+                let nonCategoryInsets = CGFloat(4)
+                let itemLeadingAndTrailingInsets = nonCategoryInsets + remainingWidth
+                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: itemLeadingAndTrailingInsets, bottom: 0, trailing: itemLeadingAndTrailingInsets)
+                
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(44))
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.boundarySupplementaryItems = [headerItem]
+                //section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 12)
+                
+                
+                return section
+            
+            }
+            
+        }
+        
+        
+       return layout
+    }
+    
+    func configureDataSource() {
+        // MARK: Data Source Initialisation
+        dataSource = .init(collectionView: collectionView, cellProvider: { (collectionView, indexPath, item) -> UICollectionViewCell  in
+            let section = self.sections[indexPath.section]
+            switch section{
+            case .promoted:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PromotedAppCollectionViewCell.reuseIdentifier, for: indexPath) as! PromotedAppCollectionViewCell
+                cell.configureCell(item.app!)
+                return cell
+            case .standard:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StandardAppCollectionViewCell.reuseIdentifier, for: indexPath) as! StandardAppCollectionViewCell
+                let isThirdItem = (indexPath.row + 1).isMultiple(of: 3) //multiple of index count
+                cell.configureCell(item.app!, hideBottomLine: isThirdItem)
+                return cell
+            case .categories:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionViewCell.reuseIdentifier, for: indexPath) as! CategoryCollectionViewCell
+                let isLastItem = collectionView.numberOfItems(inSection: indexPath.section) == indexPath.row + 1
+                cell.configureCell(item.category!, hideBottomLine: isLastItem)
+           return cell
+            }
+        })
+        
+        //MARK: Supplementary View Provider
+        dataSource.supplementaryViewProvider = { (collectionView, kind, indexPath) -> UICollectionReusableView? in
+            switch kind{
+            case SupplementaryViewKind.header:
+                
+                let section = self.sections[indexPath.section]
+                let sectionName: String
+                switch section {
+                case .promoted:
+                    return nil
+                case .standard(let name):
+                    sectionName = name
+                case .categories:
+                    sectionName = "Top Categories"
+                }
+                
+                let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: SupplementaryViewKind.header, withReuseIdentifier: SectionHeaderView.reuseIdentifier, for: indexPath) as! SectionHeaderView
+                headerView.setTitle(sectionName)
+                return headerView
+                
+            case SupplementaryViewKind.topLine, SupplementaryViewKind.bottomLine:
+                let lineView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: LineView.reuseIdentifier, for: indexPath) as! LineView
+                return lineView
+                
+            default:
+                return nil
+            }
+        }
+        
+        //MARK: Snapshot Definition
+        var snapshot = NSDiffableDataSourceSnapshot<Section,Item>()
+        snapshot.appendSections([.promoted])
+        snapshot.appendItems(Item.promotedApps, toSection: .promoted)
+        
+        let popularSection = Section.standard("Popular Section")
+        let essentialSection = Section.standard("essential Section")
+        snapshot.appendSections([popularSection, essentialSection])
+        snapshot.appendItems(Item.popularApps, toSection: popularSection)
+        snapshot.appendItems(Item.essentialApps, toSection: essentialSection)
+        
+        snapshot.appendSections([.categories])
+        snapshot.appendItems(Item.categories, toSection: .categories)
+        
+        sections = snapshot.sectionIdentifiers
+        dataSource.apply(snapshot)
+        
+    }
+}
+
